cafc19058bfbeb84a67ff669bc6c85e205037b01
Haocen Xu <admin@haocen.me>
2023-08-25 16:29:24 -0400

    Add missing value to type definition

--- src/dark-mode-toggle.d.ts
+++ src/dark-mode-toggle.d.ts
@@ -13,7 +13,7 @@ export class DarkModeToggle extends HTMLElement {
    * The "switch" appearance conveys the idea of a theme switcher (light/dark),
    * whereas "toggle" conveys the idea of a dark mode toggle (on/off).
    */
-  appearance: 'toggle' | 'switch';
+  appearance: 'toggle' | 'switch' | 'slider';
 
   /**
    * If true, remember the last selected mode ("dark" or "light"),
